e56f5bbf04fe47f8891e450db80d9e10aaa2b85a
geekyfaahad <77067677+geekyfaahad@users.noreply.github.com>
2024-12-08 22:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ async def results():
         return render_template("error.html", error_message="An error occurred while fetching results.")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True,port=55100,host='0.0.0.0')
 
 
 # httpx code
